cf9cd0672989358b138892845a5ea99cc75fabcd
urboytong <78425589+urboytong@users.noreply.github.com>
2022-02-24 23:27:16 +0700

    tong 2327

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -44,7 +44,7 @@ const Header = () => {
                       </div>
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                          <i className="fas fa-chart-bar" />
+                          <i className="ni ni-book-bookmark" />
                         </div>
                       </Col>
                     </Row>
@@ -73,8 +73,8 @@ const Header = () => {
                         </span>
                       </div>
                       <Col className="col-auto">
-                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                          <i className="fas fa-chart-bar" />
+                        <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
+                          <i className="ni ni-laptop" />
                         </div>
                       </Col>
                     </Row>
@@ -103,8 +103,8 @@ const Header = () => {
                         </span>
                       </div>
                       <Col className="col-auto">
-                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                          <i className="fas fa-chart-bar" />
+                        <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
+                          <i className="ni ni-compass-04" />
                         </div>
                       </Col>
                     </Row>
@@ -133,8 +133,8 @@ const Header = () => {
                         </span>
                       </div>
                       <Col className="col-auto">
-                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                          <i className="fas fa-chart-bar" />
+                        <div className="icon icon-shape bg-primary text-white rounded-circle shadow">
+                          <i className="ni ni-hat-3" />
                         </div>
                       </Col>
                     </Row>
@@ -165,7 +165,7 @@ const Header = () => {
                         </span>
                       </div>
                       <Col className="col-auto">
-                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                        <div className="icon icon-shape bg-green text-white rounded-circle shadow">
                           <i className="fas fa-chart-bar" />
                         </div>
                       </Col>
@@ -195,8 +195,38 @@ const Header = () => {
                         </span>
                       </div>
                       <Col className="col-auto">
-                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                          <i className="fas fa-chart-bar" />
+                        <div className="icon icon-shape bg-gray text-white rounded-circle shadow">
+                          <i className="ni ni-app" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-success mr-2">
+                        Professor : Wittawin Susutti
+                      </span>{" "}
+                     
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row >
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Software Engineer
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          CSS 111
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-pink text-white rounded-circle shadow">
+                          <i className="ni ni-box-2" />
                         </div>
                       </Col>
                     </Row>
@@ -226,37 +256,7 @@ const Header = () => {
                       </div>
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                          <i className="fas fa-chart-bar" />
-                        </div>
-                      </Col>
-                    </Row>
-                    <p className="mt-3 mb-0 text-muted text-sm">
-                      <span className="text-success mr-2">
-                        Professor : Wittawin Susutti
-                      </span>{" "}
-                     
-                    </p>
-                  </CardBody>
-                </Card>
-              </Col>
-              <Col lg="6" xl="3">
-                <Card className="card-stats mb-4 mb-xl-0">
-                  <CardBody>
-                    <Row >
-                      <div className="col">
-                        <CardTitle
-                          tag="h5"
-                          className="text-uppercase text-muted mb-0"
-                        >
-                          Software Engineer
-                        </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">
-                          CSS 111
-                        </span>
-                      </div>
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                          <i className="fas fa-chart-bar" />
+                          <i className="ni ni-calendar-grid-58" />
                         </div>
                       </Col>
                     </Row>
