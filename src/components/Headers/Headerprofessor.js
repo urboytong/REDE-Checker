bbf0db419451f293b3bbabdc794666fce093d783
urboytong <78425589+urboytong@users.noreply.github.com>
2022-02-25 15:15:47 +0700

    3:15

---
+++ src/components/Headers/Headerprofessor.js
@@ -0,0 +1,280 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+// reactstrap components
+import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+
+const Header2 = () => {
+  return (
+    <>
+      <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+        <Container fluid>
+          <div className="header-body">
+            {/* Card stats */}
+            <Row className="mt-5">
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row >
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Software Engineer
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          CSS 111
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                          <i className="ni ni-book-bookmark" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-yellow mr-2">
+                        MONDAY : 9:00 - 12:00
+                      </span>{" "}
+                     
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row >
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Software Engineer
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          CSS 111
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
+                          <i className="ni ni-laptop" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-pink mr-2">
+                      TUSEDAY : 9:00 - 12:00
+                      </span>{" "}
+                     
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row >
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Software Engineer
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          CSS 111
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
+                          <i className="ni ni-compass-04" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-orange mr-2">
+                        THURSDAY : 9:00 - 12:00
+                      </span>{" "}
+                     
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row >
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Software Engineer
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          CSS 111
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-primary text-white rounded-circle shadow">
+                          <i className="ni ni-hat-3" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-orange mr-2">
+                      THURSDAY : 13:00 - 16:00
+                      </span>{" "}
+                     
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+            <Row className="mt-2">
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row >
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Software Engineer
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          CSS 111
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-green text-white rounded-circle shadow">
+                          <i className="fas fa-chart-bar" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-success mr-2">
+                      WEDNESDAY  : 9:00 - 12:00
+                      </span>{" "}
+                     
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row >
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Software Engineer
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          CSS 111
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-gray text-white rounded-circle shadow">
+                          <i className="ni ni-app" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-success mr-2">
+                      WEDNESDAY  : 13:00 - 16:00
+                      </span>{" "}
+                     
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row >
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Software Engineer
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          CSS 111
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-pink text-white rounded-circle shadow">
+                          <i className="ni ni-box-2" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-info mr-2">
+                      FRIDAY  : 9:00 - 12:00
+                      </span>{" "}
+                     
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row >
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Software Engineer
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          CSS 111
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                          <i className="ni ni-calendar-grid-58" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-info mr-2">
+                      FRIDAY  : 13:00 - 16:00
+                      </span>{" "}
+                     
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Header2;
