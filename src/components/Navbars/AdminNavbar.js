a85a9949377230676a86d6aa31f535258537a988
plunchyy <59905929+plunchyy@users.noreply.github.com>
2022-03-09 22:53:04 +0700

    hour learned + profile + join class component

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -36,7 +36,14 @@ import {
   Modal,
   ModalBody,
   ModalFooter,
+  Row,
+  Col,
+  Button,
+  Card,
+  CardHeader,
+  CardBody
 } from "reactstrap";
+import "assets/scss/argon-dashboard/custom/AdminNavbar.scss";
 
 const AdminNavbar = (props) => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -73,18 +80,18 @@ const AdminNavbar = (props) => {
               </DropdownToggle>
               <DropdownMenu className="dropdown-menu-arrow" right>
         
-                <DropdownItem to="/admin/user-profile" tag={Link}>
+                <DropdownItem to="/admin/user-profile" tag={Link} onClick={() => setModalOpen(!modalOpen)}>
                   <i className="ni ni-single-02" />
-                  <span>My profile</span>
+                  <span >My profile</span>
                 </DropdownItem>
-                <DropdownItem to="/admin/user-profile" tag={Link}>
+                {/* <DropdownItem to="/admin/user-profile" tag={Link}>
                   <i className="ni ni-fat-add" />
                   <span>Create Class</span>
                 </DropdownItem>
                 <DropdownItem to="/admin/user-profile" tag={Link}>
                   <i className="ni ni-key-25" />
                   <span>Join Class</span>
-                </DropdownItem>
+                </DropdownItem> */}
                 
                 <DropdownItem divider />
                 <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
@@ -96,6 +103,81 @@ const AdminNavbar = (props) => {
           </Nav>
         </Container>
       </Navbar>
+
+      <Modal
+        toggle={() => setModalOpen(!modalOpen)}
+        isOpen={modalOpen}
+      >
+        <div className=" modal-header">
+          <button
+            aria-label="Close"
+            className=" close"
+            type="button"
+            onClick={() => setModalOpen(!modalOpen)}
+          >
+            <span aria-hidden={true}>×</span>
+          </button>
+        </div>
+        <ModalBody className="profileModal">
+          <Col className="order-xl-2 mb-5 mb-xl-0" xl="12">
+            <Card className="card-profile shadow">
+              <Row className="justify-content-center">
+                <Col className="order-lg-2" lg="3">
+                  <div className="card-profile-image">
+                    <a
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <img
+                        alt="..."
+                        className="rounded-circle img-profileModal"
+                        src={
+                          require("../../assets/img/theme/team-4-800x800.jpg")
+                            .default
+                        }
+                      />
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+              <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4"></CardHeader>
+              <CardBody className="pt-0 pt-md-4">
+                <Row>
+                  <div className="col">
+                    <div className="card-profile-stats d-flex justify-content-center stdID-profileModal">
+                      <div>
+                        <h2 className="heading">61090500411</h2>
+                      </div>
+                    </div>
+                  </div>
+                </Row>
+                <div className="text-center">
+                  <h2>Natthaphat Wannawat</h2>
+                  <div className="h3 font-weight-300">
+                    <i className="ni location_pin mr-2" />
+                    Science, Mathematics
+                  </div>
+                  <Button
+                    color="dark"
+                    href="#pablo"
+                    size="sm"
+                    className="edit-profile"
+                    onClick={() => setModalOpen(!modalOpen)}
+                  >
+                    Edit Profile
+                  </Button>
+                  <hr className="my-4" />
+                  <div className="h5 mt-4">
+                    <i className="ni business_briefcase-24 mr-2" />
+                    Natthaphat.tong@mail.kmutt.ac.th
+                  </div>
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </ModalBody>
+        <ModalFooter></ModalFooter>
+      </Modal>
     </>
   );
 };
