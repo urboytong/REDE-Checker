a64a1a36714e26129d64cbbfb90d52a387daa53a
bio10600 <bio10600@gmail.com>
2022-03-31 19:30:29 +0700

    Student-ClickIntoClassRoom, ClassRoomPermission

--- src/components/Headers/UserHeader2.js
+++ src/components/Headers/UserHeader2.js
@@ -1,3 +1,7 @@
+import { useState, useEffect, useContext } from "react";
+import firebaseApp from "../../firebase";
+import { useLocation, Redirect } from "react-router-dom";
+import { AuthContext } from "components/Auth/Auth.js";
 // import { Button, Container, Row, Col,Modal, ModalBody, ModalFooter, } from "reactstrap";
 import {
   Button,
@@ -30,11 +34,65 @@ import {
   UncontrolledTooltip,
 } from "reactstrap";
 
-import React, { useState } from "react";
 import "assets/scss/argon-dashboard/custom/UserHeader2.scss";
 
 const UserHeader = () => {
   const [modalOpen, setModalOpen] = useState(false);
+
+  const [ClassRoom, setClassRoom] = useState({});
+  const [Permission, setPermission] = useState(true);
+
+  const location = useLocation();
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (currentUser) {
+      //ใช้ firebaseApp.auth().onAuthStateChanged เพื่อใช้ firebaseApp.auth().currentUser โดยไม่ติด error เมื่อทำการ signout
+      firebaseApp.auth().onAuthStateChanged((user) => {
+        const db = firebaseApp.firestore();
+        const userCollection = db
+          .collection("ClassRoom")
+          .where("__name__", "==", location.search.substring(1));
+
+        // subscription นี้จะเกิด callback กับทุกการเปลี่ยนแปลงของ collection Food
+        const unsubscribe = userCollection.onSnapshot((ss) => {
+          // ตัวแปร local
+          let ClassRoom;
+
+          ss.forEach((document) => {
+            // manipulate ตัวแปร local
+            ClassRoom = document.data();
+          });
+          // เปลี่ยนค่าตัวแปร state
+          if (ClassRoom) {
+            ClassRoom.ClassDate = ClassRoom.ClassDate.toUpperCase();
+            setClassRoom(ClassRoom);
+            setPermission(
+              ClassRoom.Members.includes(currentUser._delegate.uid)
+            );
+            console.log(ClassRoom.Members.includes(currentUser._delegate.uid));
+          }
+          if (!ClassRoom) {
+            setPermission(false);
+          }
+        });
+
+        return () => {
+          // ยกเลิก subsciption เมื่อ component ถูกถอดจาก dom
+          unsubscribe();
+        };
+      });
+    }
+  }, []);
+
+  if (Permission == false) {
+    return <Redirect to="/student/student-home" />;
+  }
+
+  if (location.search.substring(1) == "") {
+    return <Redirect to="/student/student-home" />;
+  }
 
   return (
     <>
@@ -56,27 +114,25 @@ const UserHeader = () => {
         <Container className="d-flex align-items-center  subject-detail" fluid>
           <Row>
             <Col lg="7" md="10">
-            <div className="mb-5 time-sec2">
-                <span className="text-white">
-                Section 2
-                </span>
+              <div className="mb-5 time-sec2">
+                <span className="text-white">Section 2</span>
                 <span className="text-white mt-0 subject-date-time">
-                  MONDAY 9.00-12.00 A.M.
+                  {ClassRoom.ClassDate} {ClassRoom.StartTime} -{" "}
+                  {ClassRoom.EndTime} A.M.
                 </span>
               </div>
-            <h1 className=" text-white subjectCode-userHeader">
-              CSS 111
-                <br/>
+              <h1 className=" text-white subjectCode-userHeader">
+                {ClassRoom.SubjectCode}
+                <br />
               </h1>
               <h1 className="display-2 text-white subject-name">
-                Software Engineer
+                {ClassRoom.SubjectName}
               </h1>
               <div className="mb-5 time-sec">
-                <span className="text-white">
-                Section 2
-                </span>
+                <span className="text-white">Section 2</span>
                 <span className="text-white mt-0 subject-date-time">
-                  MONDAY 9.00-12.00 A.M.
+                  {ClassRoom.ClassDate} {ClassRoom.StartTime} -{" "}
+                  {ClassRoom.EndTime} A.M.
                 </span>
               </div>
               <Button
@@ -102,7 +158,7 @@ const UserHeader = () => {
         <ModalBody>
           {" "}
           <span className="font-weight-light confirm-leaveRoom text-center">
-            Do you want to leave <br/>
+            Do you want to leave <br />
             <span className="font-weight-bold">CSS 111</span>
             &nbsp;
             <span className="font-weight-bold">Software Engineer</span>
