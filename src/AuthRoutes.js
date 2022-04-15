c807d1e6cca01fb08bb88cdd6b967ef68cd92eef
plunchyy <59905929+plunchyy@users.noreply.github.com>
2022-04-16 02:15:58 +0700

    tutorial page

--- src/AuthRoutes.js
+++ src/AuthRoutes.js
@@ -20,6 +20,7 @@ import Profile from "views/examples/Profile.js";
 
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
+import Tutorial from "views/examples/Tutorial.js";
 
 import Quest from "views/examples/Quest.js";
 import FaceRegis from "views/examples/FaceRegis.js";
@@ -39,6 +40,13 @@ var routes = [
     name: "Register",
     icon: "ni ni-circle-08 text-pink",
     component: Register,
+    layout: "/auth",
+  },
+  {
+    path: "/tutorial",
+    name: "Tutorial",
+    icon: "fa-book-open-reader text-blue",
+    component: Tutorial,
     layout: "/auth",
   },
 ];
