99d3f119822f0b2f1fe955a445a1b4976e682e9d
bio10600 <bio10600@gmail.com>
2022-04-24 17:47:12 +0700

    Finished Classroom, Add+Edit Semester, Fix BackGround Height

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -41,6 +41,9 @@ import JoinClass from "components/Headers/JoinClass.js";
 const Header = () => {
   const db = firebaseApp.firestore();
   const userCollection = db.collection("ClassRoom");
+  const d = new Date();
+  let Year = d.getFullYear();
+  let Month = d.getMonth() + 1;
 
   const [ClassRoom, setClassRoom] = useState({});
   const [EmptyClassRoom, setEmptyClassRoom] = useState(false);
@@ -66,27 +69,116 @@ const Header = () => {
   };
 
   useEffect(() => {
+    //ใช้ firebaseApp.auth().onAuthStateChanged เพื่อใช้ firebaseApp.auth().currentUser โดยไม่ติด error เมื่อทำการ signout
+    if (firebaseApp.auth().currentUser) {
+      firebaseApp.auth().onAuthStateChanged((user) => {
+        const db = firebaseApp.firestore();
+        const userCollection = db
+          .collection("Quest")
+          .where("EndTimeStamp", ">=", Date.now());
 
-        //ใช้ firebaseApp.auth().onAuthStateChanged เพื่อใช้ firebaseApp.auth().currentUser โดยไม่ติด error เมื่อทำการ signout
-        if (firebaseApp.auth().currentUser) {
+        // subscription นี้จะเกิด callback กับทุกการเปลี่ยนแปลงของ collection Food
+        const unsubscribe = userCollection.onSnapshot((ss) => {
+          // ตัวแปร local
+          let CurrentQuest = [];
 
+          ss.forEach((document) => {
+            // manipulate ตัวแปร local
+            CurrentQuest.push(document.data().ClassRoomId);
+          });
+          // เปลี่ยนค่าตัวแปร state
+          setCurrentQuest(CurrentQuest);
+        });
+
+        return () => {
+          // ยกเลิก subsciption เมื่อ component ถูกถอดจาก dom
+          unsubscribe();
+        };
+      });
+    }
+  }, []);
+
+  useEffect(() => {
     firebaseApp.auth().onAuthStateChanged((user) => {
-      const db = firebaseApp.firestore();
       const userCollection = db
-        .collection("Quest")
-        .where("EndTimeStamp", ">=", Date.now());
+        .collection("ClassRoom")
+        .where("Members", "array-contains", firebaseApp.auth().currentUser.uid);
 
       // subscription นี้จะเกิด callback กับทุกการเปลี่ยนแปลงของ collection Food
       const unsubscribe = userCollection.onSnapshot((ss) => {
         // ตัวแปร local
-        let CurrentQuest = [];
+        const ClassRoom = [];
+        let count = 0;
+        let ayearcheck;
+        let semestercheck;
+
+        if (Month >= 1 && Month <= 6) {
+          semestercheck = 2;
+          ayearcheck = Year - 1;
+        }
+        if (Month >= 7 && Month <= 12) {
+          semestercheck = 1;
+          ayearcheck = Year;
+        }
 
         ss.forEach((document) => {
           // manipulate ตัวแปร local
-            CurrentQuest.push(document.data().ClassRoomId)
+          if (document.data().AcademicYear > ayearcheck) {
+            ClassRoom[count] = document.data();
+            ClassRoom[count].key = document.id;
+            ClassRoom[count].daycolor = DaysColor[ClassRoom[count].ClassDate];
+            if (document.data().Request.length == 0) {
+              ClassRoom[count].RequestBool = false;
+            }
+            if (document.data().Request.length != 0) {
+              ClassRoom[count].RequestBool = true;
+            }
+            count++;
+          }
+          if (document.data().AcademicYear == ayearcheck) {
+            if (document.data().Semester > semestercheck) {
+              ClassRoom[count] = document.data();
+              ClassRoom[count].key = document.id;
+              ClassRoom[count].daycolor = DaysColor[ClassRoom[count].ClassDate];
+              if (document.data().Request.length == 0) {
+                ClassRoom[count].RequestBool = false;
+              }
+              if (document.data().Request.length != 0) {
+                ClassRoom[count].RequestBool = true;
+              }
+              count++;
+            }
+            if (document.data().Semester == semestercheck) {
+              ClassRoom[count] = document.data();
+              ClassRoom[count].key = document.id;
+              ClassRoom[count].daycolor = DaysColor[ClassRoom[count].ClassDate];
+              if (document.data().Request.length == 0) {
+                ClassRoom[count].RequestBool = false;
+              }
+              if (document.data().Request.length != 0) {
+                ClassRoom[count].RequestBool = true;
+              }
+              count++;
+            }
+          }
         });
+
         // เปลี่ยนค่าตัวแปร state
-        setCurrentQuest(CurrentQuest);
+        ClassRoom.sort((a, b) =>
+          a.SubjectCode > b.SubjectCode
+            ? 1
+            : b.SubjectCode > a.SubjectCode
+            ? -1
+            : 0
+        );
+        setClassRoom(ClassRoom);
+        console.log(ClassRoom);
+        if (ClassRoom.length == 0) {
+          setEmptyClassRoom(true);
+        }
+        if (ClassRoom.length != 0) {
+          setEmptyClassRoom(false);
+        }
       });
 
       return () => {
@@ -94,135 +186,95 @@ const Header = () => {
         unsubscribe();
       };
     });
-
-    }
-  }, []);
-
-  useEffect(() => {
-      firebaseApp.auth().onAuthStateChanged((user) => {
-        const userCollection = db
-          .collection("ClassRoom")
-          .where(
-            "Members",
-            "array-contains",
-            firebaseApp.auth().currentUser.uid
-          );
-  
-        // subscription นี้จะเกิด callback กับทุกการเปลี่ยนแปลงของ collection Food
-        const unsubscribe = userCollection.onSnapshot((ss) => {
-          // ตัวแปร local
-          const ClassRoom = [];
-          let count = 0;
-  
-          ss.forEach((document) => {
-            // manipulate ตัวแปร local
-            ClassRoom[count] = document.data();
-            ClassRoom[count].key = document.id;
-            ClassRoom[count].daycolor = DaysColor[ClassRoom[count].ClassDate];
-            ClassRoom[count].CurrentQuestBool = false;
-            if (CurrentQuest.includes(document.id)){
-              ClassRoom[count].CurrentQuestBool = true;
-            }
-            count++;
-          });
-  
-          // เปลี่ยนค่าตัวแปร state
-          ClassRoom.sort((a, b) =>
-            a.SubjectCode > b.SubjectCode
-              ? 1
-              : b.SubjectCode > a.SubjectCode
-              ? -1
-              : 0
-          );
-          setClassRoom(ClassRoom);
-          console.log(ClassRoom);
-          if(ClassRoom.length == 0){
-            setEmptyClassRoom(true);
-          }
-          if(ClassRoom.length != 0){
-            setEmptyClassRoom(false);
-          }
-        });
-  
-        return () => {
-          // ยกเลิก subsciption เมื่อ component ถูกถอดจาก dom
-          unsubscribe();
-        };
-      });
-    
   }, [CurrentQuest]);
 
   return (
     <>
       <div className="header bg-gradient-info pb-8 pt-5 pt-md-8 bg-home-student">
         <Container fluid>
-        {!EmptyClassRoom ? (<div className="text-right">
-          <Row>
-            <Col>
-              <h1 className="classroom-active-text text-white text-left">Active Classroom</h1>
-            </Col>
-              <JoinClass />
-          </Row>
-          </div>) : null}
+          {!EmptyClassRoom ? (
+            <div className="text-right">
+              <Row>
+                <Col>
+                  <h1 className="classroom-active-text text-white text-left">
+                    Active Classroom
+                  </h1>
+                </Col>
+                <JoinClass />
+              </Row>
+            </div>
+          ) : null}
           <div className="header-body">
             {/* Card stats */}
-            {EmptyClassRoom ? (<Row>
-              <h2 className="text-home">Let's join the classroom and start to do Quest Check.</h2>
-              <div className="btn-joinclass-home-std text-home">
-                <JoinClass />
-              </div>
-            </Row>) : null}
-            <Row className="row-student-home classroom-active">
-              {Object.keys(ClassRoom).map((id) => {
-                return (
-                  <Col lg="6" xl="3">
-                    <Card
-                      className="card-stats mb-4 mb-xl-0  border-card-home"
-                      onClick={() => routeChange(ClassRoom[id].key)}
-                      style={{
-                        borderColor: ClassRoom[id].daycolor,
-                      }}
-                    >
-                      <CardBody className="subject-card">
-                        <Row>
-                          <div className="col">
-                            <CardTitle
-                              tag="h5"
-                              className="text-uppercase text-muted mb-0 home-subjectName"
-                            >
-                              {ClassRoom[id].SubjectName}
-                            </CardTitle>
-                            <span className="h2 font-weight-bold mb-0">
-                              {ClassRoom[id].SubjectCode}
+            {EmptyClassRoom ? (
+              <Row>
+                <h2 className="text-home">
+                  Let's join the classroom and start to do Quest Check.
+                </h2>
+                <div className="btn-joinclass-home-std text-home">
+                  <JoinClass />
+                </div>
+              </Row>
+            ) : null}
+            {ClassRoom.length != 0 ? (
+              <Row className="row-student-home classroom-active">
+                {Object.keys(ClassRoom).map((id) => {
+                  return (
+                    <Col lg="6" xl="3">
+                      <Card
+                        className="card-stats mb-4 mb-xl-0  border-card-home"
+                        onClick={() => routeChange(ClassRoom[id].key)}
+                        style={{
+                          borderColor: ClassRoom[id].daycolor,
+                        }}
+                      >
+                        <CardBody className="subject-card">
+                          <Row>
+                            <div className="col">
+                              <CardTitle
+                                tag="h5"
+                                className="text-uppercase text-muted mb-0 home-subjectName"
+                              >
+                                {ClassRoom[id].SubjectName}
+                              </CardTitle>
+                              <span className="h2 font-weight-bold mb-0">
+                                {ClassRoom[id].SubjectCode}
+                              </span>
+                            </div>
+                            {ClassRoom[id].CurrentQuestBool ? (
+                              <Col className="col-auto">
+                                <div
+                                  className="icon icon-shape text-white rounded-circle shadow circle-day"
+                                  style={{
+                                    backgroundColor: ClassRoom[id].daycolor,
+                                  }}
+                                >
+                                  <i class="fa-solid fa-hourglass sand-clock-icon" />
+                                </div>
+                              </Col>
+                            ) : null}
+                          </Row>
+                          <p className="mt-3 mb-0 text-muted text-sm">
+                            <span className="mr-2">
+                              {" "}
+                              {ClassRoom[id].ClassDate} :{" "}
+                              {ClassRoom[id].StartTime} -{" "}
+                              {ClassRoom[id].EndTime} &nbsp; Semester :{" "}
+                              {ClassRoom[id].Semester}/
+                              {ClassRoom[id].AcademicYear}
                             </span>
-                          </div>
-                          {ClassRoom[id].CurrentQuestBool ? (
-                            <Col className="col-auto">
-                              <div
-                                className="icon icon-shape text-white rounded-circle shadow circle-day"
-                                style={{
-                                  backgroundColor: ClassRoom[id].daycolor,
-                                }}
-                                
-                              ><i class="fa-solid fa-hourglass sand-clock-icon"/></div>
-                            </Col>
-                          ) : null}
-                        </Row>
-                        <p className="mt-3 mb-0 text-muted text-sm">
-                          <span className="mr-2">
-                            {" "}
-                            {ClassRoom[id].ClassDate} :{" "}
-                            {ClassRoom[id].StartTime} - {ClassRoom[id].EndTime}
-                          </span>
-                          <span className="mr-2 section">Sec : {ClassRoom[id].Section}</span>{" "}
-                        </p>
-                      </CardBody>
-                    </Card>
-                    &nbsp;
-                  </Col>
-                );
-              })}
-            </Row>
+                            <span className="mr-2 section">
+                              Sec : {ClassRoom[id].Section}
+                            </span>{" "}
+                          </p>
+                        </CardBody>
+                      </Card>
+                      &nbsp;
+                    </Col>
+                  );
+                })}
+              </Row>
+            ) : null}
           </div>
         </Container>
       </div>
